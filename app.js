63cdbca3395cd96e2e6e1d925b4d23c1010f96db
Elias Teikari <elias.tkri@gmail.com>
2025-10-26 02:39:12 +0300

    OS Node module

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const path = require("path");
+const os = require("os");
 
-let pathObj = path.parse(__filename);
-console.log(pathObj);
+let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Total Memory: ${freeMemory}`);
