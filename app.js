7e30937b62b01ce73cbf687e26ee04f4ac40972d
Elias Teikari <elias.tkri@gmail.com>
2025-10-26 02:57:21 +0300

    Event emitter arguments

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 // register a listener
-emitter.on("messageLogged", function () {
-  console.log("Listener called");
+emitter.on("messageLogged", (arg) => {
+  console.log("Listener called", arg);
 });
 
 // Raise an event, but not a listener, that handles when that event is raised
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
+
+// Raise: logging (data: message)
+emitter.on("loggingEvent", (arg) => {
+  console.log("Logger: ", arg);
+});
+
+emitter.emit("loggingEvent", { id: 1, logging: "error message" });
